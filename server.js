e7267c212af3137b050fad9c447a1516149ecb20
arpansadhu10 <74260413+arpansadhu10@users.noreply.github.com>
2023-03-01 02:01:24 +0530

    Update server.js
    changed port to 8000

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT||8000;
+console.log(PORT)
 const server = app.listen(PORT, () => {
   console.log(`Server running on port:${PORT}`);
 });
