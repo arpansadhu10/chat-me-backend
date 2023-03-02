8e13adee9d79e7792595c03a3ffbfc030fccedfb
arpan <arpansadhu03@gmail.com>
2023-03-02 21:44:25 +0530

    refactored cors link in socket.io

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: ["http://localhost:3000", "https://web-chatme.netlify.app", "https://chatme.streamify.co.in/"],
+    origin: ["http://localhost:3000", "https://web-chatme.netlify.app", "https://chatme.streamify.co.in"],
   },
 });
 
